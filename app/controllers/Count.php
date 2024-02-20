9785dfc2096311fa84fd1f55f66e2dace075a67e
Hussainamin10 <hussain.amin@icloud.com>
2024-02-19 22:36:51 -0500

    Created count controller class

---
+++ app/controllers/Count.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+class Count{
+
+function index(){
+    $counter = new \app\models\Counter();
+    $counter->increment();
+    $counter->write();
+    echo $counter;
+    
+}
+}
