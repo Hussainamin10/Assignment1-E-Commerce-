d2144551d08211330d3d340f1949988152a56211
Sheldon Green <2076213@edu.vaniercollege.qc.ca>
2024-02-20 01:48:31 -0500

    Working on About us and Contact us page

---
+++ app/views/Main/about_us.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title><?= $name ?> view</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+<ul>
+  <li><a class="active" href="index">About Us</a></li>
+  <li><a href="index">About us</a></li>
+  <li><a href="contact">Contact us</a></li>
+  <li><a href="about">See the messages we get</a></li>
+</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Example</title>
+</head>
+<body>
+<img src="image.jpg" alt="Description of the image">
+
+</body>
+</html>
+</body>
+</html>
